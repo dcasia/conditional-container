61b4f5203ad9ccf1c692e294c091032071e1db1d
milewski <rafael.milewski@gmail.com>
2019-10-22 20:12:01 +0800

    fix issue with belongsToMany relationship type

--- src/HasConditionalContainer.php
+++ src/HasConditionalContainer.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Collection;
 use Laravel\Nova\Fields\FieldCollection;
 use Laravel\Nova\Http\Controllers\ActionController;
 use Laravel\Nova\Http\Controllers\AssociatableController;
-use Laravel\Nova\Http\Controllers\AttachableController;
 use Laravel\Nova\Http\Controllers\CreationFieldController;
 use Laravel\Nova\Http\Controllers\FieldController;
 use Laravel\Nova\Http\Controllers\MorphableController;
@@ -152,7 +151,7 @@ trait HasConditionalContainer
                  * If instance of any associative type flatten out all the fields
                  */
                 if ($controller instanceof AssociatableController ||
-                    $controller instanceof AttachableController ||
+//                    $controller instanceof AttachableController ||
                     $controller instanceof MorphableController ||
                     $controller instanceof ResourceAttachController ||
                     $controller instanceof FieldController) {
